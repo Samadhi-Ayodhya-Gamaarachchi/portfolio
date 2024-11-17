082725d3f183e742308ad052cb4949bcfe99ae71
Samadhi Ayodhya Gamaarachchi <samadhigamaarachchi07@gmail.com>
2024-11-17 22:13:03 +0530

    header

---
+++ portfolio/src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import NavBar from '../NavBar/NavBar'
+import '../Header/Header.css'
+
+function Header() {
+  return (
+    <>
+    <div className='nav'>
+    <div className='logo'>
+    <h3>Samadhi Gamaarachchi</h3>
+    </div>
+    
+      <NavBar linkname="Home" url="/"/>
+      <NavBar linkname="About me" url="/about"/>
+      <NavBar linkname="Projects" url="/projects"/>
+      <NavBar linkname="Contact me" url="/contact"/>
+    </div>
+    </>
+  )
+}
+
+export default Header
