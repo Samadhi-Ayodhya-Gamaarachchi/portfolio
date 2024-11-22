65174296274e4665af423bdbec52fd3d17a61d69
Samadhi Ayodhya Gamaarachchi <samadhigamaarachchi07@gmail.com>
2024-11-22 09:31:32 +0530

    fixed nav-bar

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -9,11 +9,12 @@ function Header() {
     <div className='logo'>
     <h3>Samadhi Gamaarachchi</h3>
     </div>
-    
+    <div className='item'>
       <NavBar linkname="Home" url="/"/>
       <NavBar linkname="About me" url="/about"/>
       <NavBar linkname="Projects" url="/projects"/>
       <NavBar linkname="Contact me" url="/contact"/>
+      </div>
     </div>
     </>
   )
