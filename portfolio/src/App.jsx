3eedf5f646127302022077e34bbdde458dd906cc
Samadhi Ayodhya Gamaarachchi <samadhigamaarachchi07@gmail.com>
2024-11-22 10:13:33 +0530

    edit

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,18 +1,21 @@
 import "./App.css";
+import ButtonContact from "./components/button(contactme)/ButtonContact";
 import Header from "./components/Header/Header";
-import cover from "./assets/cover.png"
+
 
 function App() {
-  
-
-  
-
-
-
   return (
     <>
-      
+      <div className="hello">
         <Header />
+        <div className="name">
+          <p>Hi! I'm Samadhi Gamaarachchi,</p>
+        
+          <p className="passion">Software Engineering Undergraduate</p>
+          <ButtonContact/>
+        </div>
+
+        </div>
         
     </>
   );
